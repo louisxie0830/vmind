c46c4d815bbda0cfa9eae2307845375b7d0b40f4
Louis <bfgh2678@gmail.com>
2019-06-12 16:49:15 +0800

    create vue prj

---
+++ precache-manifest.6f9ce03647484959e1aebfadd57b3615.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "777bc487d33e48865976",
+    "url": "js/chunk-vendors.79c13957.js"
+  },
+  {
+    "revision": "73aed481f824e23baebf",
+    "url": "js/app.0c0611a8.js"
+  },
+  {
+    "revision": "3eec4a656019d40079c7122d49f7809c",
+    "url": "index.html"
+  },
+  {
+    "revision": "0a267abaf4c9d30b8fd7c77cc19625f3",
+    "url": "img/zwicon.0a267aba.svg"
+  },
+  {
+    "revision": "d7e439cec483969db045235b2bd3a748",
+    "url": "fonts/zwicon.d7e439ce.ttf"
+  },
+  {
+    "revision": "cbeae0335fc8290a7611dd00a840391f",
+    "url": "fonts/zwicon.cbeae033.woff"
+  },
+  {
+    "revision": "64bead59d217d53c5331dcb3d7687814",
+    "url": "fonts/zwicon.64bead59.eot"
+  },
+  {
+    "revision": "777bc487d33e48865976",
+    "url": "css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "73aed481f824e23baebf",
+    "url": "css/app.94c70681.css"
+  }
+];
